6e12fd3970687dcf8568a248c32aee1f4cc6c8e8
Dilshan0212 <136323281+Dilshan0212@users.noreply.github.com>
2024-01-14 12:50:40 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+startClassification() 
+{
+ml5.soundClassifier()
+}
+
+modelReady()
+{
+    classifier.classify()
+}
+
+gotResults()
+{
+    if (error) {
+        
+    } 
+    else (results) {
+    
+
+    
+    }
+}
